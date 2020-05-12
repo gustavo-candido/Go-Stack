4f5e190e4b12ca820b06958c046ab1a2e0aef0a6
gustavo-candido <gustavocandidofreitas@gmail.com>
2020-05-12 18:43:41 -0300

    hello world

---
+++ conceitos-dev/mobile/src/app.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import  { View, Text, StyleSheet, StatusBar } from 'react-native';
+
+export default function App() {
+    return (
+        <>
+            <StatusBar barStyle='light-content' backgroundColor='red'/>
+            <View style={styles.container}>
+                <Text style={styles.title}>Hello World</Text>
+            </View>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#7159c1",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    title: {
+        color: '#fff',
+        fontSize: 32,
+        fontWeight: "bold",
+    }
+});
